03aee3d3983144f676a885d7c1377dfd75b4be3f
miniotti <robin.miniotti@etu.unistra.fr>
2025-10-30 15:50:35 +0100

    feat: updated credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvac9c44c73d0019g-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollutions_user",
+    "password" : "1X9lkb5069FJKU7ZgfcIM31hwaA9n9LP",
+    "bdname" :"pollutions" 
     }
 }
 
