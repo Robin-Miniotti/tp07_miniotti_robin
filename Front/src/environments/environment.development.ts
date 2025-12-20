7b054036d88e309c4e5f2ba364c6597ae03a71d8
miniotti <robin.miniotti@etu.unistra.fr>
2025-12-20 18:47:00 +0100

    update

--- Front/src/environments/environment.development.ts
+++ Front/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/assets/mocks/pollutions.json'
+  apiUrl: 'http://localhost:3000/api'
 };
--- Front/src/environments/environment.ts
+++ Front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://apitemplate-latest-c47a.onrender.com/api' 
+    apiUrl: 'https://apitemplate-latest-c47a.onrender.com/api'
 };
